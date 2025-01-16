7c17ca495382f704fee7c8497b8982c842bf2160
al18nna <melnik.alena2006@gmail.com>
2025-01-16 14:58:31 +0300

    Работа со свзанным списком

---
+++ Program1.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+
+class Program1
+{
+    static void Main()
+    {
+        string filePath = "C:\\Users\\melni\\OneDrive\\Рабочий стол\\input.txt";
+
+
+        LinkedList<string> lines = new LinkedList<string>();
+
+        Stopwatch stopWatch = new Stopwatch();
+        stopWatch.Start();
+        try
+        {
+            foreach (string line in File.ReadLines(filePath))
+            {
+                lines.AddLast(line);
+            }
+
+            foreach (string line in lines)
+            {
+                Console.WriteLine(line);
+            }
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine("Произошла ошибка: " + ex.Message);
+        }
+        stopWatch.Stop();
+        TimeSpan ts = stopWatch.Elapsed;
+
+        string elapsedTime = String.Format("{0:00}:{1:00}:{2:00}.{3:00}",
+            ts.Hours, ts.Minutes, ts.Seconds,
+            ts.Milliseconds / 10);
+        Console.WriteLine("RunTime " + elapsedTime);
+        Console.ReadKey();
+    }
+}
