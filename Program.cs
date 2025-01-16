54cef5c032d1bf556f0b8667d6208a07e35b31cb
al18nna <melnik.alena2006@gmail.com>
2025-01-16 14:50:58 +0300

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+
+class Program
+{
+    static void Main()
+    {
+        string filePath = "C:\\Users\\melni\\OneDrive\\Рабочий стол\\input.txt";
+
+
+        List<string> lines = new List<string>();
+
+        Stopwatch stopWatch = new Stopwatch();
+        stopWatch.Start();
+        try
+        {
+            foreach (string line in File.ReadLines(filePath))
+            {
+                lines.Add(line);
+            }
+
+            foreach (string line in lines)
+            {
+                Console.WriteLine(line);
+            }
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine("Произошла ошибка: " + ex.Message);
+        }
+        stopWatch.Stop();
+        TimeSpan ts = stopWatch.Elapsed;
+
+        string elapsedTime = String.Format("{0:00}:{1:00}:{2:00}.{3:00}",
+            ts.Hours, ts.Minutes, ts.Seconds,
+            ts.Milliseconds / 10);
+        Console.WriteLine("RunTime " + elapsedTime);
+    }
+}
